612a4fc355002fb95858b88a440238137aaf69b3
daisuke524 <daisuke.u524@gmail.com>
2020-06-22 21:48:00 +0900

    WorkListViewを削除

--- app/views.py
+++ app/views.py
@@ -11,13 +11,6 @@ class IndexView(View):
             'work_data': work_data
         })
 
-class WorkListView(View):
-    def get(self, request, *args, **kwargs):
-        work_data = Work.objects.order_by("-id")
-        return render(request, 'app/work_list.html', {
-            'work_data': work_data
-        })
-
 class WorkDetailView(View):
     def get(self, request, *args, **kwargs):
         work_data = Work.objects.get(id=self.kwargs['pk'])
