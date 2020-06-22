027e3a22e80ce09d3cb3122bed4b418d08251c0e
daisuke524 <daisuke.u524@gmail.com>
2020-06-22 21:47:34 +0900

    workを削除

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,5 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('work', views.WorkListView.as_view(), name='work_list'),
     path('work-detail/<int:pk>/', views.WorkDetailView.as_view(), name='work_detail'),
 ]
