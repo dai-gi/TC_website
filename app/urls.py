522757e584bb38be4a874f61ca317ae75ab2a6b0
daisuke524 <daisuke.u524@gmail.com>
2020-06-29 06:58:08 +0900

    ブログ詳細画面のURLを追加

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('post/<int:pk>/', views.IndexView.as_view(), name='index'),
     path('work-detail/<int:pk>/', views.WorkDetailView.as_view(), name='work_detail'),
 ]
