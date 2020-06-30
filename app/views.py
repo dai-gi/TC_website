c3ff217242eac1d8fe45fea6c185e7f0fe417a3b
daisuke524 <daisuke.u524@gmail.com>
2020-07-01 07:06:29 +0900

    投稿機能追加

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from .forms import PostForm
 from app.models import Post, Work
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from djnago.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -9,6 +11,28 @@ class IndexView(View):
         return render(request, 'app/index.html', {
             'post_data': post_data,
             'work_data': work_data
+        })
+class CreatePostView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+        
+        return render(request, 'app/post_form.html', {
+            'form': form
+        })
+
+        def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        if form.is_valid():
+            post_data = Post()
+            post_data.author = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.content = form.cleaned_data['content']
+            post_data.save()
+            return redirect('post_detail', post_data.id)
+
+        return render(request, 'app/post_form.html', {
+            'form': form
         })
 
 
