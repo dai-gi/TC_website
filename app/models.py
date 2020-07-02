441af5064c9a25e9cafb7622b55ebe45d6af2373
daisuke524 <daisuke.u524@gmail.com>
2020-07-03 07:07:06 +0900

    workの項目追加

--- app/models.py
+++ app/models.py
@@ -20,8 +20,11 @@ class Post(models.Model):
 
 class Work(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	title = models.CharField('案件', max_length=200)
-	text = models.TextField('詳細')
+	title = models.CharField('タイトル', max_length=200)
+	address = models.CharField('現場住所', max_length=200)
+	content = models.TextField('作業内容', blank=True, null=True)
+	staff = models.CharField('現場担当者', max_length=200, blank=True, null=True)
+	tel = models.CharField('電話番号', max_length=200, blank=True, null=True)
 	price = models.IntegerField('価格')
 	created_date = models.DateTimeField('作成日', default=timezone.now)
 	published_date = models.DateTimeField('公開日', blank=True, null=True)
