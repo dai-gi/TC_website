f73f295c6e5ba6fc604d0aecddf2ac07a90b9bb5
daisuke524 <daisuke.u524@gmail.com>
2020-07-04 14:08:06 +0900

    content修正

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,13 @@
 from django import forms
+# from .models import Post
 
 class PostForm(forms.Form):
     title = forms.CharField(max_length=30, label='タイトル')
-    content = forms.CharField(label='内容', widget=forms.Textarea())
+    text = forms.CharField(label='内容', widget=forms.Textarea())
+
+    # class Meta:
+    #     model = Post
+    #     fields = '__all__'
+    #     widgets = {
+    #         'created_at': forms.SelectDateWidget
+    #     }
