e5252fc8f24242a5800330662d627b55e1e4797c
daisuke524 <daisuke.u524@gmail.com>
2020-07-09 13:04:15 +0900

    画像アップロード機能追加

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	title = models.CharField('タイトル', max_length=200)
+	image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True) 
 	text = models.TextField('内容')
 	created_date = models.DateTimeField('作成日', default=timezone.now)
 	published_date = models.DateTimeField('公開日', blank=True, null=True)
