5d227109029fee88d5b70e0de385d1f16fbc569d
daisuke524 <daisuke.u524@gmail.com>
2020-07-12 23:56:27 +0900

    カテゴリ機能追加

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Work
+from .models import Post, Work, Category
 
 admin.site.register(Post)
 admin.site.register(Work)
+admin.site.register(Category)
