af628df58e6e6d4f390c7f4df0ccbce9dc43653e
daisuke524 <daisuke.u524@gmail.com>
2020-08-04 21:40:01 +0900

    workのカテゴリ機能を追加

--- app/forms.py
+++ app/forms.py
@@ -13,13 +13,13 @@ class PostForm(forms.Form):
 
 
 class WorkForm(forms.Form):
-    # category_data = Category.objects.all()
-    # category_choice = {}
-    # for category in category_data:
-    #     category_choice[category] = category
+    category_data = Category.objects.all()
+    category_choice = {}
+    for category in category_data:
+        category_choice[category] = category
 
     title = forms.CharField(max_length=30, label='タイトル')
-    # category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items())) 
+    category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items())) 
     address = forms.CharField(label='内容', max_length=30)
     text = forms.CharField(label='内容', widget=forms.Textarea())
     staff = forms.CharField(label='内容', max_length=30)
