a4e631d74bc18e7a690eb336d5437e7df3e2998a
daisuke524 <daisuke.u524@gmail.com>
2020-08-18 07:24:53 +0900

    カテゴリを分別

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Work, Category
+from .models import Post, Work, PostCategory, WorkCategory
 
 admin.site.register(Post)
 admin.site.register(Work)
-admin.site.register(Category)
+admin.site.register(PostCategory)
+admin.site.register(WorkCategory)
 
--- app/models.py
+++ app/models.py
@@ -4,7 +4,14 @@ from django.utils import timezone
 from django.urls import reverse
 
 
-class Category(models.Model):
+class PostCategory(models.Model):
+    name = models.CharField("カテゴリ", max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class WorkCategory(models.Model):
     name = models.CharField("カテゴリ", max_length=50)
 
     def __str__(self):
@@ -13,7 +20,7 @@ class Category(models.Model):
 
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT) 
+	post_category = models.ForeignKey(PostCategory, verbose_name='カテゴリ', on_delete=models.PROTECT) 
 	title = models.CharField('タイトル', max_length=200)
 	image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True) 
 	text = models.TextField('内容')
@@ -29,7 +36,7 @@ class Post(models.Model):
 
 class Work(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT) 
+	work_category = models.ForeignKey(WorkCategory, verbose_name='カテゴリ', on_delete=models.PROTECT) 
 	title = models.CharField('タイトル', max_length=200)
 	address = models.CharField('現場住所', max_length=200)
 	text = models.TextField('作業内容', blank=True, null=True)
