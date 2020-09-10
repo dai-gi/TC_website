f6a194cb2fcfd7a6fcb6bf9579c0b7f321a0e4c7
daisuke524 <daisuke.u524@gmail.com>
2020-09-10 23:47:16 +0900

    work_commonを追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'app.context_processors.common',
+                'app.context_processors.work_common',
             ],
         },
     },
