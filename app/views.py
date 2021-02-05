8b03585fd1a44d606b0300118e63e0071cb56cfc
dai-gi <daisuke.u524@gmail.com>
2021-02-05 23:28:24 +0900

    修正

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render, redirect
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         post_data = Post.objects.order_by("-id")
-
         return render(request, 'app/index.html', {
             'post_data': post_data,
         })
